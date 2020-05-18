9a4f8a4bec6238c9a7b9e9e4f7ab7ab7259ac8f4
Dima Oksenenko <Oksenenko@organosyn.local>
2020-05-18 11:10:57 +0300

    v2

--- application.py
+++ application.py
@@ -110,8 +110,8 @@ welcome = """
 </head>
 <body id="sample">
   <div class="textColumn">
-    <h1>Congratulations</h1>
-    <p>Your first AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
+    <h1>organosyn site</h1>
+    <p>Hi from oksenenko dmitriy</p>
   </div>
   
   <div class="linksColumn"> 
